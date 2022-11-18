cf3854012613fb56744fd478fe3f2e8e5a2794a5
ragunath arjun <ragunath219@gmail.com>
2022-11-18 17:22:41 +0530

    Update

--- React/nov18/src/App.js
+++ React/nov18/src/App.js
@@ -7,12 +7,13 @@ function App() {
     <Counter />
   )
 }
-
 function Counter(){
   let[Like,setLike]=useState(10);
   let[Dislike,setDislike]=useState(0);
   return(
     <>
+    <button onClick={()=>setLike(Like+1)}> hi{Like} </button>
+    <button onClick={()=>setDislike(Like+1)}> hi{Dislike} </button>
     <button onClick={()=>setLike(Like+1)}> hi{Like} </button>
     <button onClick={()=>setDislike(Like+1)}> hi{Dislike} </button>
     </>
